0e91f4574b12804e164028de017f63587945834e
Julia <Julia@DESKTOP-674032J>
2022-11-05 10:32:31 -0600

    Control 1 - pc

---
+++ ConsoleApp1/Repositories/IMarca.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Repositories
+{
+    public interface IMarca
+    {
+        public int MyProperty();
+    }
+}
