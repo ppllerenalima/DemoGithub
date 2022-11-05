949dd7fb698b65f08da64253c1db7adace3b3371
pllerena_laptop <pplle@SERVER01>
2022-11-05 11:36:25 -0500

    Control 1 - Laptop

---
+++ ConsoleApp1/Repositories/IProductoge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Repositories
+{
+    public interface IProductoge
+    {
+        public bool Update();
+    }
+}
---
+++ ConsoleApp1/Repositories/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Repositories
+{
+    public class Marca : IMarca
+    {
+        public int MyProperty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
